40df9a3cd51a1e2fcb148332e8163130bee47678
sudhanshugupta <sudhanshugupta@localhost>
2020-04-25 10:28:07 +0530

    initial checkin

---
+++ src/main/java/com/javainuse/SpringBootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.javainuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass=true)
+public class SpringBootHelloWorldApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+}
